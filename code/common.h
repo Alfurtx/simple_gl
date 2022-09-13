5af2dbe49d084d62103286809f5429af40026a02
Alfonso Amorós Cubells <alfonso.alfurtx@gmail.com>
2022-09-13 22:46:35 +0200

    chore: changed typedefs names

--- code/common.h
+++ code/common.h
@@ -9,26 +9,25 @@
 #include <stdint.h>
 
 // NOTE(fonsi): Graphics library includes
-// TODO(fonsi): In the far future this should be substituted by a from scratch version
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
 
-typedef int8_t i8;
-typedef int16_t i16;
-typedef int32_t i32;
-typedef int64_t i64;
+typedef int8_t  int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
 
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
+typedef uint8_t  uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
 
-typedef float f32;
-typedef double f64;
+typedef float  float32;
+typedef double float64;
 
-typedef bool b32;
+typedef bool bool32;
 
 typedef unsigned int uint;
 
